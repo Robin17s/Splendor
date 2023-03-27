819fbb2316b87578f07987fa9da8de87d5c5ab46
TheAlgorithm476 <thealgorithm476@gmail.com>
2023-03-27 13:03:01 +0200

    Modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module splendor {
+    exports cui;
+    exports domain;
+    exports gui;
+    exports main;
+
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+
+    opens gui to javafx.graphics;
+}
