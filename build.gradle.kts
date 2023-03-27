38abe309ecdbc6c96ed6630aba38728b33031d15
TheAlgorithm476 <thealgorithm476@gmail.com>
2023-03-27 12:57:02 +0200

    Add JavaFX

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,27 @@
 plugins {
     id("java")
+    id("application")
+    id("org.javamodularity.moduleplugin") version "1.8.12"
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
 group = "splendor-g-53"
 version = "1.0-SNAPSHOT"
 
-repositories() {
+repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.mysql:mysql-connector-j:8.0.31")
+    implementation("com.mysql:mysql-connector-j:8.0.32")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+javafx {
+    version = "17.0.2"
+    modules = arrayListOf("javafx.controls", "javafx.fxml")
 }
