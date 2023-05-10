d16a23c5422da312429b71778c91dbe190cbd9de
Brent Van den Abbeel <brent.vandenabbeel@student.hogent.be>
2023-05-10 08:59:44 +0200

    Wrote Javadocs for main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,15 @@ package main;
 
 import gui.ApplicationStart;
 
+/**
+ * Main entrypoint for Splendor's GUI Application.
+ * <p>
+ * This class contains one single method, being {@link #main(String[])}, solely responsible for launching the GUI components of the game.
+ */
 public class Main {
+    /**
+     * Responsible for starting the GUI Section of Splendor. This method will call the GUI main method.
+     * @param args The JVM startup args
+     */
     public static void main(String[] args) { ApplicationStart.main(args); }
 }
